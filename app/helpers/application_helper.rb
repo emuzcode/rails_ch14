f354022d012f71d664ac9b9ada7f70668fc44d45
Taro Sakazume <ts95.business@gmail.com>
2022-06-16 10:05:14 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  
+  # Title of Static Pages
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
+  class ConsoleTest
+    # 4.10 回文かどうかをチェックするメソッド
+    def palindrome_tester(s)
+      if s == s.reverse
+        puts "It's a palindrome!"
+      else
+        puts "It's not a palindrome."
+      end
+    end
+  end
 end
