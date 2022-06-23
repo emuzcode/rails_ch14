3fd6316bc8e9e222ea3f2db18cbf55614fe743f3
Taro Sakazume <ts95.business@gmail.com>
2022-06-23 07:39:11 +0000

    add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ Rails.application.routes.draw do
   
   # users
   #resources :users
-    get '/users',          to: 'users#index', as: 'users'
-    get '/users/:id',      to: 'users#show',  as: 'user'
-    get '/signup',         to: 'users#new'
-    post '/users',         to: 'users#create'
-    get '/users/:id/edit', to: 'users#edit',  as: 'edit_user'
-    patch '/users/:id',    to: 'users#update'
-    delete '/users/:id',   to: 'users#destroy'
+    get    '/users',          to: 'users#index', as: 'users'
+    get    '/users/:id',      to: 'users#show',  as: 'user'
+    get    '/signup',         to: 'users#new'
+    post   '/users',          to: 'users#create'
+    get    '/users/:id/edit', to: 'users#edit',  as: 'edit_user'
+    patch  '/users/:id',      to: 'users#update'
+    delete '/users/:id',      to: 'users#destroy'
     
   # sessions
   get    '/login',   to: 'sessions#new'
@@ -24,4 +24,9 @@ Rails.application.routes.draw do
   
   # account_activation
   resources :account_activations, only: [:edit]
+  
+  # password_resets
+  resources :password_resets, only: [:new, :create, :edit, :update]
+    #get '/password_resets/new', to: 'password_resets#new', as: 'new_password_reset'
+    #post '/password_resets', to: 'password_resets#create'
 end
